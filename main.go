605f6d2efd9d098d76f035f9c2b3a21c6e22868d
Jon Pepler <jonathan.pepler@gmail.com>
2022-08-31 22:51:58 +0100

    report zones with errors

--- main.go
+++ main.go
@@ -6,29 +6,29 @@ import (
 	"sort"
 )
 
-// var sudoku = [][]int{
-// 	{8, 2, 9, 3, 6, 5, 1, 4, 7},
-// 	{6, 4, 3, 7, 1, 8, 5, 1, 9},
-// 	{7, 5, 1, 4, 9, 2, 6, 3, 8},
-// 	{3, 1, 8, 5, 2, 7, 4, 9, 6},
-// 	{5, 9, 6, 1, 4, 1, 7, 8, 2},
-// 	{4, 7, 2, 9, 8, 6, 3, 1, 5},
-// 	{9, 8, 4, 6, 5, 1, 2, 7, 3},
-// 	{2, 6, 7, 8, 3, 4, 9, 5, 1},
-// 	{9, 3, 5, 2, 7, 9, 8, 6, 4},
-// }
-
 var sudoku = [][]int{
-	{1, 5, 2, 4, 8, 9, 3, 7, 6},
-	{7, 3, 9, 2, 5, 6, 8, 4, 1},
-	{4, 6, 8, 3, 7, 1, 2, 9, 5},
-	{3, 8, 7, 1, 2, 4, 6, 5, 9},
-	{5, 9, 1, 7, 6, 3, 4, 2, 8},
-	{2, 4, 6, 8, 9, 5, 7, 1, 3},
-	{9, 1, 4, 6, 3, 7, 5, 8, 2},
-	{6, 2, 5, 9, 4, 8, 1, 3, 7},
-	{8, 7, 3, 5, 1, 2, 9, 6, 4},
+	{8, 2, 9, 3, 6, 5, 1, 4, 7},
+	{6, 4, 3, 7, 1, 8, 5, 1, 9},
+	{7, 5, 1, 4, 9, 2, 6, 3, 8},
+	{3, 1, 8, 5, 2, 7, 4, 9, 6},
+	{5, 9, 6, 1, 4, 1, 7, 8, 2},
+	{4, 7, 2, 9, 8, 6, 3, 1, 5},
+	{9, 8, 4, 6, 5, 1, 2, 7, 3},
+	{2, 6, 7, 8, 3, 4, 9, 5, 1},
+	{9, 3, 5, 2, 7, 9, 8, 6, 4},
 }
+
+// var sudoku = [][]int{
+// 	{1, 5, 2, 4, 8, 9, 3, 7, 6},
+// 	{7, 3, 9, 2, 5, 6, 8, 4, 1},
+// 	{4, 6, 8, 3, 7, 1, 2, 9, 5},
+// 	{3, 8, 7, 1, 2, 4, 6, 5, 9},
+// 	{5, 9, 1, 7, 6, 3, 4, 2, 8},
+// 	{2, 4, 6, 8, 9, 5, 7, 1, 3},
+// 	{9, 1, 4, 6, 3, 7, 5, 8, 2},
+// 	{6, 2, 5, 9, 4, 8, 1, 3, 7},
+// 	{8, 7, 3, 5, 1, 2, 9, 6, 4},
+// }
 
 func main() {
 	validate_sudoku(sudoku)
@@ -40,23 +40,26 @@ func validate_sudoku(sudoku [][]int) {
 	grids := grid_sudoku(sudoku)
 
 	valid := true
-	for _, row := range rows {
+	for i, row := range rows {
 		valid_zone := check_zone((row))
-		if valid_zone == false {
+		if !valid_zone {
+			fmt.Println("Error on row", i)
 			valid = false
 		}
 	}
 
-	for _, column := range columns {
+	for i, column := range columns {
 		valid_zone := check_zone((column))
-		if valid_zone == false {
+		if !valid_zone {
+			fmt.Println("Error on column", i)
 			valid = false
 		}
 	}
 
-	for _, grid := range grids {
+	for i, grid := range grids {
 		valid_zone := check_zone((grid))
-		if valid_zone == false {
+		if !valid_zone {
+			fmt.Println("Error on grid", i)
 			valid = false
 		}
 	}
